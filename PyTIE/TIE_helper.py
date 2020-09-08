0de62f0fed45449bdd01ca94283152f73367dbb4
Art-MC <arthurmccray2018@u.northwestern.edu>
2020-09-08 14:20:56 -0500

    adding scale option for show_im

--- PyTIE/TIE_helper.py
+++ PyTIE/TIE_helper.py
@@ -8,6 +8,7 @@ Author: Arthur McCray, ANL, Summer 2019.
 """
  
 import matplotlib.pyplot as plt 
+import matplotlib as mpl
 import numpy as np
 import hyperspy.api as hs
 from skimage import io
@@ -451,7 +452,8 @@ def scale_stack(imstack):
 to have handy when working in Jupyter notebooks."""
 
 
-def show_im(image, title=None, simple=False, origin='upper', cbar=True, cbar_title='', **kwargs):
+def show_im(image, title=None, simple=False, origin='upper', cbar=True,
+    cbar_title='', scale=None, **kwargs):
     """Display an image on a new axis.
     
     Takes a 2D array and displays the image in grayscale with optional title on 
@@ -476,6 +478,8 @@ def show_im(image, title=None, simple=False, origin='upper', cbar=True, cbar_tit
             simple = False. 
         cbar_title (str): (`optional`) Title attached to the colorbar (indicating the 
             units or significance of the values). 
+        scale (float): Scale of image in nm/pixel. Axis markers will be given in
+            units of nanometers. 
 
     Returns:
         None
@@ -493,16 +497,36 @@ def show_im(image, title=None, simple=False, origin='upper', cbar=True, cbar_tit
         ax.set_title(str(title), pad=0)
 
     if simple:
-        # plt.axis('off')
-        pass
+        plt.axis('off')
     else:
         plt.tick_params(axis='x',top=False)
         ax.xaxis.tick_bottom()
         ax.tick_params(direction='in')
+        if scale is None:
+            ticks_label = 'pixels'
+        else:
+            def mjrFormatter(x, pos):
+                return f"{scale*x:.3g}"
+
+            fov = scale * max(image.shape[0], image.shape[1])
+            print('fov is (nm): ', fov)
+
+            if fov < 4e3: # if fov < 4um use nm scale
+                ticks_label = ' nm '
+            elif fov > 4e6: # if fov > 4mm use m scale
+                ticks_label = "  m  "
+                scale /= 1e9
+            else: # if fov between the two, use um
+                ticks_label = " $\mu$m "
+                scale /= 1e3
+
+            ax.yaxis.set_major_formatter(mpl.ticker.FuncFormatter(mjrFormatter))
+            ax.xaxis.set_major_formatter(mpl.ticker.FuncFormatter(mjrFormatter))
+
         if origin == 'lower': 
-            ax.text(y=0,x=0,s='pixels', rotation=-45, va='top', ha='right')
-        elif origin =='upper': 
-            ax.text(y=image.shape[0],x=0,s='pixels', rotation=-45, va='top', ha='right')
+            ax.text(y=0,x=0,s=ticks_label, rotation=-45, va='top', ha='right')
+        elif origin =='upper': # keep label in lower left corner
+            ax.text(y=image.shape[0],x=0,s=ticks_label, rotation=-45, va='top', ha='right')
 
         if cbar: 
             plt.colorbar(im, ax=ax, pad=0.02, format="%.2g", label=str(cbar_title))
